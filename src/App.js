bce98000c99743c90e2e7d98cc1d12b484e5a7fc
Walid <wmahersia@gmail.com>
2020-04-11 18:52:49 +0200

    Quete 3 créer et affiche un composant ContactList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 
 function App() {
   return (
     <div className="App">
-      <Contact avatar="https://i.pinimg.com/originals/47/da/56/47da56e41f6a2984d102877c580b315d.jpg" name="Walter White" online />
+      {/* <Contact avatar="https://i.pinimg.com/originals/47/da/56/47da56e41f6a2984d102877c580b315d.jpg" name="Walter White" online />
       <Contact avatar="https://cdna.artstation.com/p/assets/images/images/013/585/110/large/abdelrahman-kubisi-main-final.jpg?1540281037" name="Mike"  />
-      <Contact avatar="https://cdn-biiinge.konbini.com/files/2017/04/gusrfring.jpg" name="Gus"  />
+      <Contact avatar="https://cdn-biiinge.konbini.com/files/2017/04/gusrfring.jpg" name="Gus"  /> */}
+
+
+
+
+      <ContactList />
     </div> 
   );
 }
